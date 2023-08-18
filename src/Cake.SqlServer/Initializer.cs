a1fc2908d0f0b3c1f31efc491ba1b6fe0fee3dc2
Reza Ebrahimi <ebrahimi.esf@gmail.com>
2023-08-18 23:46:57 +0330

    Removed unused code that was from NET461 builds

--- src/Cake.SqlServer/Initializer.cs
+++ src/Cake.SqlServer/Initializer.cs
@@ -1,39 +1,10 @@
-﻿#if NET461
-using System;
-using System.IO;
-using System.Reflection;
-#endif
-
-namespace Cake.SqlServer
+﻿namespace Cake.SqlServer
 {
     internal static class Initializer
     {
-#if NET461
-        private static bool _initialized;
-#endif
-
         internal static void InitializeNativeSearchPath()
         {
-#if NET461
-            if (Environment.OSVersion.Platform == PlatformID.Win32NT)
-            {
-                if (_initialized)
-                {
-                    return;
-                }
-
-                var codeBase = Assembly.GetExecutingAssembly().CodeBase;
-                var uri = new UriBuilder(codeBase);
-                var path = Uri.UnescapeDataString(uri.Path);
-                var addinFolder = Path.GetDirectoryName(path);
-                NativeMethods.SetDefaultDllDirectories(NativeMethods.LOAD_LIBRARY_SEARCH_DEFAULT_DIRS);
-                NativeMethods.AddDllDirectory(addinFolder);
-
-                _initialized = true;
-            }
-#else
             // initialization not necessary
-#endif
         }
     }
 }
